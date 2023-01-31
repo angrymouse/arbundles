27e26ca606f1c34c47b6841061eeb766fa04c318
Angrymouse <rykov.nikas@gmail.com>
2023-01-31 21:06:29 +0200

    node

--- src/ar-data-base.ts
+++ src/ar-data-base.ts
@@ -2,7 +2,7 @@ import { stringToBuffer } from "arweave/node/lib/utils";
 import DataItem from "./DataItem";
 import { isBrowser } from "./utils";
 import { deepHash } from "./deepHash";
-import webDeepHash from "arweave/web/lib/deepHash";
+import webDeepHash from "arweave/node/lib/deepHash";
 /**
  * Options for creation of a DataItem
  */
